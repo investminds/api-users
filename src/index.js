1439adc3649795c15f1bb1db36af0839c51b1f6c
Nicalo Ribeiro <nicalo63@gmail.com>
2024-05-07 11:59:38 -0300

    feat: base routes created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger.js";
+import assistantRouter from "./assistant/assistant-router.js";
+import raceRouter from "./race/race-router.js";
 
 const app = express();
 const port = process.env.PORT || 9123;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use("/api/assistant", assistantRouter);
+app.use("/api/race", raceRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}!`);
