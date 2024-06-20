2910b1adae4f994085e4ede9cf3f3f812108b161
Nicalo Ribeiro <nicalo63@gmail.com>
2024-06-20 14:52:08 -0300

    feat: routes added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.js";
 import assistantRouter from "./assistant/assistant-router.js";
 import raceRouter from "./race/race-router.js";
+import { registerRouter } from "./user/user-router.js";
+import facebookRouter from "./facebook/facebook-router.js";
 
 const app = express();
 const port = process.env.PORT || 9123;
@@ -15,6 +17,9 @@ app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api/assistant", assistantRouter);
 app.use("/api/race", raceRouter);
+
+app.use("/api/user", registerRouter);
+app.use("/api/facebook", facebookRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}!`);
